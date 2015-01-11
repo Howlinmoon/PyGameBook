ef30522ad2d37c927ac8318b0c947f70a9b7b52b
Jim Veneskey <github@strifecrafter.com>
2015-01-11 16:09:36 -0500

    PyGame Book Chapter 4, listing 4-8, Random pixel generation, locking

--- Chapter04_08.py
+++ Chapter04_08.py
@@ -3,7 +3,7 @@ from pygame.locals import *
 from sys import exit
 from random import randint
 
-# Random Pixel generation, non-locking
+# Random Pixel generation, locking
 
 pygame.init()
 
@@ -17,10 +17,13 @@ while True:
             quit()
     
     rand_color = (randint(0,255), randint(0,255), randint(0,255))
+    # Lock the screen
+    screen.lock()
     for _ in xrange(100):
         rand_pos = (randint(0,639), randint(0, 479))
         screen.set_at(rand_pos, rand_color)
-        
+    # don't forget to unlock it
+    screen.unlock()  
     pygame.display.update()
     
     
